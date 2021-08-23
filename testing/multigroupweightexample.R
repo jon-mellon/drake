0308b78f2a2682efd43b6c85656be6b217abdee6
Jon Mellon <jonathan.mellon@nuffield.ox.ac.uk>
2021-08-23 15:27:04 -0400

    updated multigroup example code

--- testing/multigroupweightexample.R
+++ testing/multigroupweightexample.R
@@ -1,15 +1,15 @@
 library(drake)
 n.per.group <- 100
 n.groups <- 3
-real.props.per.group <- runif(n.groups, 0.1, 0.9)
+true.props.per.group <- runif(n.groups, 0.1, 0.9)
 observed.props.per.group <- runif(n.groups, 0.1, 0.9)
 true.group.sizes <- prop.table(abs(rnorm(n.groups)))
-names(real.props.per.group) <- names(observed.props.per.group) <- 
+names(true.props.per.group) <- names(observed.props.per.group) <- 
   names(true.group.sizes) <- state.abb[1:n.groups]
 
 z.true.prop <- runif(1,0.3, 0.7)
 z.observed.prop <- runif(1,0.3, 0.7)
-data <- data.frame(X = unlist(lapply(real.props.per.group, rbinom, size = 1, n=n.per.group)), 
+data <- data.frame(X = unlist(lapply(observed.props.per.group, rbinom, size = 1, n=n.per.group)), 
                    Z = rbinom(n.per.group * n.groups, prob = z.observed.prop, size = 1), 
                    group = inverse.rle(list(values = 1:n.groups, lengths = rep(n.per.group, n.groups))))
 
@@ -30,30 +30,22 @@ makeABTarget <- function(x)  {
   c(A = 1-x, B = x)
 }
 
-discrete.target.subset <- list(X = list(group = lapply(real.props.per.group, makeABTarget)  ))
+discrete.target.subset <- list(X = list(group = lapply(true.props.per.group, makeABTarget)  ))
 
-data$group<- factor(data$group, levels = c("AZ", "AL", "AK"))
+# data$group<- factor(data$group, levels = c("AZ", "AL", "AK"))
 
 
 data$weights <- drake(sample = data, discrete.targets = discrete.targets, 
-                      discrete.target.subset =discrete.target.subset)
+                      discrete.target.subset =discrete.target.subset, max.weights = 100)
 
 discrete.targets
 prop.table(wtd.table(data$group, weights = data$weights))
 prop.table(wtd.table(data$Z, weights = data$weights))
 
+hist(data$weights, 100)
 
-discrete.target.subset
-prop.table(tapply(data$weights, list(data$group, data$X), sum), 1)
-prop.table(wtd.table(data$Z, weights = data$weights))
 
-data$Y
-
-max.weights = 25
-min.weights <- 1/max.weights
-
-sample <- data
-sample$weights <- 1
-var <- "X"
-discrete.sub <- discrete.targets.subset$X
-
+group.order <- c("AL", "AK", "AZ")
+true.props.per.group[group.order]
+t(simplify2array(discrete.target.subset$X$group))[group.order, ]
+prop.table(tapply(data$weights, list(data$group, data$X), sum), 1)[group.order, ]

02ca3e3f19bc30fd80b35ba36bfc7e18cedd3361
Jonathan Mellon <jonamellon@gmail.com>
2026-04-19 10:47:49 -0400

    fixing country mapping

--- R/getMortCountries.R
+++ R/getMortCountries.R
@@ -2,7 +2,7 @@ getMortCountries <- function() {
   mort.countries <- c(AT = "AUT", AU = "AUS", BE = "BEL", BR = NA, 
                       CA = "CAN", CH = "CHE", DE = "DEUTNP", 
                       DK = "DNK", ES = "ESP", FR = "FRATNP",
-                      GB = "GBR_NP", IE =  "IRL", IL = "ISL",
+                      GB = "GBR_NP", IE =  "IRL", IL = "ISR",
                       IT = "ITA", JP = "JPN", NIR = "GBR_NIR",
                       NL = "NLD", NO = "NOR", NZ = "NZL_NP", 
                       PL = "POL", PT = "PRT", RO = "BGR", 
